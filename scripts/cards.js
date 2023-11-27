b25a04e7beee83f4ec6961096847df3a0bd85c79
joao paulo g sena <jp.provenet@gmail.com>
2023-11-27 18:27:59 -0300

    add page cards

--- scripts/cards.js
+++ scripts/cards.js
@@ -1,4 +1,64 @@
-const playrName = document.querySelector(".playerName");
-const storagePlayerName = localStorage.getItem("@memoryGame:playrName")
-playrName.innerHTML = storagePlayerName;
+function backPage() {
+  const playerResp = confirm("Deseja sair do jogo? Você perderá seu progresso!");
+  if (playerResp) {
+    window.history.back();
+  }
+}
 
+function createCards() {
+  gridCards.innerHTML = "";
+  sortedCards.forEach((card) => {
+    gridCards.innerHTML += `
+      <div class="card" name="${card}">
+        <div class="front">
+          <img src="../images/${card}.jpg" alt="">
+        </div>
+        <div class="back">
+          <img src="../images/yugioh-card-back.png" alt="">
+        </div>
+      </div>
+    `;
+  });
+}
+
+const playerName = document.querySelector(".playerName");
+const backButton = document.querySelector(".backButton");
+const gridCards = document.querySelector(".gridCards");
+
+const storagePlayerName = localStorage.getItem("@memoryGame:playerName");
+
+playerName.innerHTML = storagePlayerName;
+
+backButton.addEventListener("click", backPage);
+
+const cardNames = [
+  "card_1",
+  "card_2",
+  "card_3",
+  "card_4",
+  "card_5",
+  "card_6",
+  "card_7",
+  "card_8",
+  "card_9",
+  "card_10",
+  "card_11",
+  "card_12",
+  "card_13",
+  "card_14",
+  "card_15",
+  "card_16",
+  "card_17",
+  "card_18",
+  "card_19",
+];
+
+const arrayCardsName = cardNames
+  .sort(() => Math.random() - 0.5)
+  .filter((value, index) => index < 12);
+
+const sortedCards = [...arrayCardsName, ...arrayCardsName].sort(
+  () => Math.random() - 0.5
+);
+
+createCards();
